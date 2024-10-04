e4aeeb63956b44a673c591a7bb2ee70d7f2b0134
Julian Pernia <julian.pernia@gmail.com>
2024-10-04 13:09:01 -0400

    Begin work on Task Grid page

---
+++ TaskManager/TaskManager.Client/ModelAdapter/SelectableTask.cs
@@ -0,0 +1,26 @@
+﻿using Task = TaskManager.Shared.Models.Task;
+
+namespace TaskManager.Client.ModelAdapter
+{
+    public class SelectableTask : Task
+    {
+        public SelectableTask(Task task)
+        {
+            Id = task.Id;
+            Title = task.Title;
+            Description = task.Description;
+            Created = task.Created;
+            CreatedBy = task.CreatedBy;
+            Priority = task.Priority;
+            Status = task.Status;
+
+            IsSelected = false;
+        }
+        public bool IsSelected { get; set; } = false;
+
+        public static List<SelectableTask> ConvertToSelectable(IEnumerable<Task> tasks)
+        {
+            return tasks.Select(task => new SelectableTask(task)).ToList();
+        }
+    }
+}
