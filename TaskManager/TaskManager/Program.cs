1b0046060124490e8ddee21b76ffa85644ce7481
Julian Pernia <julian.pernia@gmail.com>
2024-10-04 12:11:19 -0400

    Create template project and add .gitignore

---
+++ TaskManager/TaskManager/Program.cs
@@ -0,0 +1,33 @@
+using TaskManager.Client.Pages;
+using TaskManager.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(TaskManager.Client._Imports).Assembly);
+
+app.Run();
