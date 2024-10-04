0149ddb39827902de3424ddbec38f31ecf188ea7
Julian Pernia <julian.pernia@gmail.com>
2024-10-04 13:30:39 -0400

    Add mapper for enumerations

--- TaskManager/TaskManager.Client/ModelAdapter/SelectableTask.cs
+++ TaskManager/TaskManager.Client/ModelAdapter/SelectableTask.cs
@@ -1,4 +1,6 @@
 ﻿using Task = TaskManager.Shared.Models.Task;
+using Status = TaskManager.Shared.Models.Status;
+using Priority = TaskManager.Shared.Models.Priority;
 
 namespace TaskManager.Client.ModelAdapter
 {
@@ -21,6 +23,23 @@ namespace TaskManager.Client.ModelAdapter
         public static List<SelectableTask> ConvertToSelectable(IEnumerable<Task> tasks)
         {
             return tasks.Select(task => new SelectableTask(task)).ToList();
+        }
+
+        public string StatusName
+        {
+            get => Status switch
+            {
+                Status.NotStarted => "Not Started",
+                Status.InProgress => "In Progress",
+                Status.Done => "Done",
+                Status.Blocked => "Blocked",
+                _ => "Error"
+            };
+        }
+
+        public string PriorityName
+        {
+            get => Priority.ToString();
         }
     }
 }
